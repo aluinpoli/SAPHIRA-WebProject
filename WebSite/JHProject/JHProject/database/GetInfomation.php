f139236f74f2eebeb0d40e39c96b5aeaa1fcfbfc
Denis <alarma7a@gmail.com>
2014-06-30 09:49:27 +0300

    added sorting by categories

--- WebSite/JHProject/JHProject/database/GetInfomation.php
+++ WebSite/JHProject/JHProject/database/GetInfomation.php
@@ -1,17 +1,41 @@
 <?php
 
 include_once 'Connection.php';
-$sql = "select * from jokes ";
+
+$sql="select * from jokes";
+if($_GET){
+    switch ($_GET['show']){
+    case "all":
+        break;
+    case "html":
+        $sql="select * from jokes where joke_sphere=1";
+        break; 
+    case "css":
+        $sql="select * from jokes where joke_sphere=2";
+        break; 
+    case "java":
+        $sql="select * from jokes where joke_sphere=3";
+        break; 
+    case "csharp":
+        $sql="select * from jokes where joke_sphere=4";
+        break; 
+    }
+}
+
 $db = new DatabaseConnect;
 $query = $db->execute($sql);
 if (isset($query)) {
+    $i=1;
     while ($row = mysqli_fetch_assoc($query)) {
-        $joke[$row['joke_id']]['title'] = $row['joke_title'];
-        $joke[$row['joke_id']]['content'] = $row['joke_content'];
-        $joke[$row['joke_id']]['date'] = $row['joke_date'];
-        $joke[$row['joke_id']]['type'] = $row['joke_type'];
-        $joke[$row['joke_id']]['sphere'] = $row['joke_sphere'];
+        $id[$i]=$row['joke_id'];
+        $joke[$id[$i]]['title'] = $row['joke_title'];
+        $joke[$id[$i]]['content'] = $row['joke_content'];
+        $joke[$id[$i]]['date'] = $row['joke_date'];
+        $joke[$id[$i]]['type'] = $row['joke_type'];
+        $joke[$id[$i]]['sphere'] = $row['joke_sphere'];
+        $i++;
     }
 }
-$num_jokes = mysqli_num_rows($query);
+
+
 
