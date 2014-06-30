5cf4260977a7acb85b5fc80720c140d1a69175a9
Denis <alarma7a@gmail.com>
2014-06-30 11:20:00 +0300

    add new database file

---
+++ WebSite/JHProject/JHProject/database/jokes.sql
@@ -0,0 +1,55 @@
+-- phpMyAdmin SQL Dump
+-- version 4.1.12
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: 
+-- Версия на сървъра: 5.6.16
+-- PHP Version: 5.5.11
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `jokehunter`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Структура на таблица `jokes`
+--
+
+CREATE TABLE IF NOT EXISTS `jokes` (
+  `joke_id` int(11) NOT NULL AUTO_INCREMENT,
+  `joke_title` text NOT NULL,
+  `joke_content` text NOT NULL,
+  `joke_date` date NOT NULL,
+  `joke_type` int(2) NOT NULL,
+  `joke_sphere` int(1) NOT NULL,
+  PRIMARY KEY (`joke_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=9 ;
+
+--
+-- Схема на данните от таблица `jokes`
+--
+
+INSERT INTO `jokes` (`joke_id`, `joke_title`, `joke_content`, `joke_date`, `joke_type`, `joke_sphere`) VALUES
+(1, 'Common Nakov`s joke', 'Изчезни бе гад мръсна', '2014-06-22', 2, 1),
+(2, 'Common Nakov`s joke', 'Изчезни бе гад мръсна', '2014-06-13', 2, 2),
+(3, 'My style', 'imgs/web-des.jpg', '2014-06-18', 1, 2),
+(4, 'My donuts', 'imgs/donuts.jpg', '2014-03-04', 1, 3),
+(5, 'Common Nakov`s joke', 'Изчезни бе гад мръсна', '2014-06-28', 2, 3),
+(6, 'Your donuts', 'imgs/donuts.jpg', '2014-07-11', 1, 4),
+(7, 'Test C#', 'Some joke about C#', '2014-06-04', 2, 4),
+(8, 'Test C#', 'Some joke about C#', '2014-06-04', 2, 4);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
